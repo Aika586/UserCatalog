1703732e363a6c689b150af99dc9d58b8209832f
Aikokul <baatirbekovna.6@mail.ru>
2024-06-27 16:42:31 +0600

    last commit

--- src/pages/signUp/SignUp.tsx
+++ src/pages/signUp/SignUp.tsx
@@ -1,11 +1,11 @@
 import SignUpForm from "../../components/Form/SignUpForm";
-import { Outlet } from "react-router-dom";
+
 const SignUp = () => {
   return (
     <>
-  <SignUpForm />;
-  <Outlet/>
-  </>)
+      <SignUpForm />;
+    </>
+  );
 };
 
 export default SignUp;
